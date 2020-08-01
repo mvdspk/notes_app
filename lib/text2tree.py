7faa1a8843f0319ea268d77626818791652dfc3b
mvdspk <123mvds@gmail.com>
2020-07-31 19:21:35 -0700

    text2tree - algo

--- lib/text2tree.py
+++ lib/text2tree.py
@@ -28,3 +28,45 @@ the first line that appears with 0 indent is the root
 subsequnet lines are considered the children
 their indent is their depth
 """
+
+"""
+ALGO
+
+fileContent <-- read the file as string
+linesList <-- split lines on line ending
+linesList <-- filter blank lines
+
+textTree <-- create a tree object
+textTree.root <-- linesLis[0] assign first element as root
+treePathStack to ctrack the path while building a tree
+
+for each children
+of the root buildTree(), buy using the stack to mimic the path traversed
+    buildTree() takes a line
+        counts the starting white spaces and considers it as depth of the line
+        uses stack length as the tree depth
+        takes the difference treedepth and node depth
+        if diff is 0 we are adding sibling
+            so pop existing sibling, pop again to get parent
+            add this node to parent as a child
+            push parent
+            push this node
+        if diff is negative
+            that means this node belongs to higher level
+            so keep popping untl the differenrece is 0, i.e. reached same level
+            again pop to get parent, 
+            add this node to parent as a child
+            push parent
+            push this node
+        if diff is positive by one unit
+            we are adding a child
+            pop parent
+            add this node to parent as child
+            push parent
+            push child
+by the end of iteration, e would have reached last line and built the last leaf of the tree
+
+
+
+
+"""
